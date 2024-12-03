5b40e50e748ed78188b1f91d20bab804a5ae08f2
CabosManuel <cabos.apolaya@gmail.com>
2024-12-03 16:35:00 -0500

    Properties read-only

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -3,5 +3,6 @@ import { Product } from "./product.model"
 export const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
+  // data.id = 'fake' // Error read-only
   products.push(data);
 }
