0443d0fda3d6173b490fe65170c5541e40f024a3
CabosManuel <cabos.apolaya@gmail.com>
2025-02-20 12:02:14 -0500

    Add FakeJS library, add more product attributes and generate 50 products

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,18 +1,28 @@
+import { faker } from "@faker-js/faker";
 import { products, addProduct } from "./product/product.service"
 
-addProduct({
-  id: "P01",
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  title: "Product 1",
-  stock: 90,
-  size: "S",
-  category: {
-    id: "C01",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    name: "Category 1"
-  }
-});
+for (let i = 0; i < 50; i++) {
+  addProduct({
+    id: faker.string.uuid(),
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.recent(),
+    title: faker.commerce.productName(),
+    stock: faker.number.int({ min: 0, max: 100 }),
+    size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+    category: {
+      id: faker.string.uuid(),
+      createdAt: faker.date.past(),
+      updatedAt: faker.date.recent(),
+      name: faker.commerce.department(),
+    },
+    image: faker.image.url({width: 500, height: 500}),
+    description: faker.commerce.productDescription(),
+    color: faker.color.human(),
+    price: parseInt(faker.commerce.price(), 10),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.uniqueArray(faker.commerce.productAdjective, 3)
+  });
+}
+
 
 console.log("🚀 ~ products:", products);
